cb4e12cbfe97c285cf1be6d87497e5bea06ab9c6
Nick <nickpowers43@gmail.com>
2016-04-27 09:55:31 -0400

    Added Region based Atmosphere simulation interfaces

---
+++ SS14.Server.Interfaces/Atmos/IAtmosRegion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS14.Server.Interfaces.Atmos
+{
+    public interface IAtmosRegion
+    {
+
+        //get pressure
+        //get temperature
+        //get concentration of gas x (H2O, O2, CO2, N2, N2O, etc...)
+    }
+}
---
+++ SS14.Server.Interfaces/Atmos/IAtmosRegionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using SS14.Server.Interfaces.Map;
+
+namespace SS14.Server.Interfaces.Atmos
+{
+    public interface IAtmosRegionManager
+    {
+        void Initialize(ISS14Server server);
+
+        IAtmosRegion RegionAt(Vector2f position);
+        void Update(float elapsedTime);
+
+    }
+}
